a64ecd3db16947bd54f18f656a58846e4515bbfd
ralphminor <ralphminor@users.noreply.github.com>
2017-07-07 09:36:11 -0600

    Connected the React App to the Redux store making booksList a smart component using Provider to access state and display the list of books.

--- reduxApp/src/app.js
+++ reduxApp/src/app.js
@@ -1,6 +1,7 @@
 "use strict"
 import React from 'react'
 import {render} from 'react-dom';
+import {Provider} from 'react-redux';
 
 import {applyMiddleware, createStore} from 'redux';
 import logger from 'redux-logger';
@@ -19,7 +20,9 @@ const store = createStore(reducers, middleware);
 import BooksList from './components/pages/bookslist';
 
 render(
-  <BooksList />, document.getElementById('app')
+  <Provider store={store}>
+    <BooksList />
+  </Provider>, document.getElementById('app')
 );
 
 // Step 2 create and dispatch actions
@@ -42,19 +45,19 @@ store.dispatch(postBook(
   }]
 ))
 
-// Delete a book
-store.dispatch(deleteBook(
-  {id: 1}
-))
+// // Delete a book
+// store.dispatch(deleteBook(
+//   {id: 1}
+// ))
 
-// Update a book
-store.dispatch(updateBook(
-  {
-    id: 2,
-    title: "Learn React and Redux"
-  }
-))
+// // Update a book
+// store.dispatch(updateBook(
+//   {
+//     id: 2,
+//     title: "Learn React and Redux"
+//   }
+// ))
 
-// -->> CART ACTIONS <<--
-// ADD to cart
-store.dispatch(addToCart([{id: 1}]));
+// // -->> CART ACTIONS <<--
+// // ADD to cart
+// store.dispatch(addToCart([{id: 1}]));
--- reduxApp/src/components/pages/bookslist.js
+++ reduxApp/src/components/pages/bookslist.js
@@ -1,13 +1,30 @@
 "use strict"
 import React from 'react';
+import {connect} from 'react-redux';
 
 class BooksList extends React.Component {
   render() {
+    const booksList = this.props.books.map(function(booksArr){
+      return(
+        <div key={booksArr.id}>
+          <h2>{booksArr.title}</h2>
+          <h2>{booksArr.description}</h2>
+          <h2>{booksArr.price}</h2>
+        </div>
+      )
+    })
     return(
       <div>
         <h1>Hello React</h1>
+        {booksList}
       </div>
     )
   }
 }
-export default BooksList;
+
+function mapStateToProps(state) {
+  return {
+    books: state.books.books
+  }
+}
+export default connect(mapStateToProps)(BooksList);
