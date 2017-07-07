7e2dde80815820b85cf00cc5847a34cac441dae9
ralphminor <ralphminor@users.noreply.github.com>
2017-07-07 10:30:34 -0600

    Added react-bootstrap and BootstrapCDN for app styling.  Added basic styling in BooksList component.

--- reduxApp/src/components/pages/bookslist.js
+++ reduxApp/src/components/pages/bookslist.js
@@ -3,6 +3,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {getBooks} from '../../actions/booksActions';
+import {Grid, Col, Row, Button} from 'react-bootstrap';
 
 class BooksList extends React.Component {
   componentDidMount() {
@@ -16,14 +17,16 @@ class BooksList extends React.Component {
           <h2>{booksArr.title}</h2>
           <h2>{booksArr.description}</h2>
           <h2>{booksArr.price}</h2>
+          <Button bsStyle="primary">Buy Now</Button>
         </div>
       )
     })
     return(
-      <div>
-        <h1>Hello React</h1>
-        {booksList}
-      </div>
+      <Grid>
+        <Row style={{marginTop:'1em'}}>
+          {booksList}
+        </Row>
+      </Grid>
     )
   }
 }
