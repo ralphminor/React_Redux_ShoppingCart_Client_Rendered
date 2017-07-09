aa25aeccf7dceba32c11b2cbeb35db7331018fbf
ralphminor <ralphminor@users.noreply.github.com>
2017-07-08 18:05:15 -0600

    Added a navbar in a new file (menu.js) and a footer in footer.js  and added the navbar and footer in app.js.  Added styling in a new file (public/style.css) and added link to style.css in index.html.

---
+++ reduxApp/src/components/footer.js
@@ -0,0 +1,15 @@
+"use strict"
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="footer text-center">
+        <div className="container">
+          <p className="footer-text">&copy; 2017 Ralph Minor. All rights reserved.</p>
+        </div>
+      </footer>
+    );
+  }
+}
+export default Footer
